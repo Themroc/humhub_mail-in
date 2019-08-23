08d9ded5efb74c9705b35426fdf9989bc69fc1f0
Themroc <themroc@69b.org>
2019-08-23 16:48:12 +0200

    Use humhub, not yii

--- views/mail_in/config.php
+++ views/mail_in/config.php
@@ -1,7 +1,7 @@
 <?php
 
-use yii\bootstrap\ActiveForm;
-use yii\helpers\Html;
+use humhub\widgets\ActiveForm;
+use humhub\libs\Html;
 use themroc\humhub\modules\mail_in\Module;
 
 echo '<div class="panel panel-default">'."\n";
@@ -13,20 +13,21 @@ $form = ActiveForm::begin(['id' => 'container-form']);
 echo '	<div class="form-group">'."\n";
 
 if ($have_maildir)
-echo '		' . $form->field($model, 'method')->radioList(Module::getMethods(), ['itemOptions'=>['class'=>'mail-in_config_method']]) . "\n";
-#echo '		' . $form->field($model, 'method')->dropDownList($model->getMethods()) . "\n";
+echo '		' . $form->field($model, 'method')->radioList(Module::getMethods(), ['itemOptions'=>['class'=>'mail-in_config_method']])."\n";
+#echo '		' . $form->field($model, 'method')->dropDownList($model->getMethods())."\n";
 
-echo '		' . $form->field($model, 'source', ['options'=>['style'=>'display:'.($model->method==Module::ACCESS_METHOD_IMAP ? "block" : "none")]]) . "\n";
-echo '		' . $form->field($model, 'maildir', ['options'=>['style'=>'display:'.($model->method==Module::ACCESS_METHOD_MAILDIR ? "block" : "none")]]) . "\n";
-echo '		' . $form->field($model, 'address') . "\n";
-echo '		' . $form->field($model, 'showaddr')->checkbox() . "\n";
+echo '		' . $form->field($model, 'source', ['options'=>['style'=>'display:'.($model->method==Module::ACCESS_METHOD_IMAP ? "block" : "none")]])."\n";
+echo '		' . $form->field($model, 'maildir', ['options'=>['style'=>'display:'.($model->method==Module::ACCESS_METHOD_MAILDIR ? "block" : "none")]])."\n";
+echo '		' . $form->field($model, 'address')."\n";
+echo '		' . $form->field($model, 'showaddr')->checkbox()."\n";
 echo '		<div class="form-group" id="group-showaddr" style="margin-left:36px; display:'.($model->showaddr ? "block" : "none").'">'."\n";
-echo '			' . $form->field($model, 'sortorder') . "\n";
+echo '			' . $form->field($model, 'sortorder')."\n";
 echo '		</div>'."\n";
 echo '	</div>'."\n";
 
 echo '	<div class="form-group">'."\n";
-echo '		' . Html::submitButton(Yii::t('base', 'Save', ['class' => 'btn btn-primary', 'data-ui-loader' => ''])) . "\n";
+echo '		' . Html::saveButton()."\n";;
+echo '		' . Html::a(Yii::t('base', 'Back'), '/admin/modules', ['class' => 'btn btn-default pull-right'])."\n";
 echo '	</div>'."\n";
 
 ActiveForm::end();
